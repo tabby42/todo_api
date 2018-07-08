20f2641dc79221df52947bc1580868f251377f35
tabby42 <irene.theiss@gmx.at>
2018-07-08 17:26:55 +0200

    send static file at root route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,23 @@
 var express = require('express'),
-    app = express();
+	app = express(),
+	bodyParser = require('body-parser');
+var todoRoutes = require('./routes/todos');
+
+// allow access to request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//access views folder
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
-	res.send('Hello from express');
+	res.sendFile('index.html');
 });
 
-app.get('/happy', function(req, res) {
-	res.send({message: ':)'});	//or res.json()
-});
+app.use('/api/todos', todoRoutes);
+
+// app.get('/happy', function(req, res) {
+// 	res.send({message: ':)'});	//or res.json()
+// });
 
 //start server
 //listen(port, host, callback)
